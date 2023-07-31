5e56b6144f881ec9eea4bab86e7ce40db68b13d4
Waruhiu <warupoems@gmail.com>
2023-08-01 00:12:34 +0200

    privacy service complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Goals App Privacy Policy</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Waruhiu built the Goals app as a Free app. This SERVICE is provided by Waruhiu at no cost and is intended for use as is.
+          This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
+          If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. I will not use or share your information with anyone except as described in this Privacy Policy.
+          The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Goals unless otherwise defined in this Privacy Policy.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Information Collection and Use</h2>
+        <p>The Goals application does not collect any Personal Information with any intention or for any purpose. Nonetheless, the following services may collect information as mentioned below.</p>
+        <p>Link to the privacy policy of third-party service providers used by the app </p><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a>
+        <h2>Cookies</h2>
+        <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
+        This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+        <h2>Links to Other Sites</h2>
+        <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+        <h2>Children's Privacy</h2>
+        <p>These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13 years of age.</p>
+        <h2>Changes to This Privacy Policy</h2>
+        <p>I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page. This policy is effective as of 2023-07-31.</p>
+        <h2>Contact Us</h2>
+        <p>If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at warupoems@gmail.com.</p>
     </div>
   );
 }
